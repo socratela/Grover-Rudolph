91dbe5e733b398f5d5829f10037008e39724c889
socratela <daniela.falco99@gmail.com>
2025-01-27 09:12:19 +0100

    Agregar función calcular_theta para calcular valores de theta a partir de una función a integrar y una malla generada

--- Function.py
+++ Function.py
@@ -69,4 +69,67 @@ x_values = [0.2, 0.4, 0.6, 0.8]
 print(f_instance(x_values))  # Evaluating f(x)
 print(f_instance([0, 1], integral=True))  # Evaluating the definite integral
 
+def calcular_theta(n, f_instance, discrete=False):
+    """
+    Calcula un conjunto de valores theta a partir de una función a integrar y una malla generada.
 
+    Parámetros:
+    - n (int): Número de niveles de la malla. Determina cuántos valores de theta calcular.
+    - f_instance (función): Función que se utilizará para calcular las integrales numéricas.
+    - discrete (bool): Indica si se utiliza un enfoque discreto para los cálculos.
+
+    Devuelve:
+    - result (lista de listas): Una lista que contiene sublistas de valores de theta calculados en cada nivel.
+    """
+    result = []  # Lista principal que contendrá las sublistas de resultados
+
+    if not discrete:
+        for l in range(1, n + 1):
+            malla = Utils.generar_puntos(l)  # Genera la malla para el nivel l
+            thetas = []  # Sublista para almacenar los valores de theta en el nivel l
+
+            for i in range(2**(l - 1)):
+                # Calcula las integrales en los intervalos correspondientes
+                num = f_instance([malla[2 * i], malla[2 * i + 1]], integral=True)
+                denom = f_instance([malla[2 * i], malla[2 * i + 2]], integral=True)
+
+                # Calcula theta usando la función arco coseno
+                if denom == 0:
+                    thetal = np.pi / 2 if malla[2 * i] == 0 else 0
+                else:
+                    thetal = np.arccos(np.sqrt(num / denom))
+
+                # Añade el valor de theta a la sublista
+                thetas.append(thetal)
+
+            # Añade la sublista de valores de theta al resultado
+            result.append(thetas)
+        return result
+
+    else:
+        for l in range(1, n + 1):
+            thetas = []  # Sublista para almacenar los valores de theta en el nivel l
+            dist_num = 2**(n - l)
+            dist_denom = 2**(n - l + 1)
+
+            for i in range(2**(l - 1)):
+                if l == n:
+                    # Calcula la integral numérica para el caso especial en el último nivel
+                    num = f_instance([i * dist_num], integral=True)
+                else:
+                    # Calcula las integrales en los intervalos correspondientes
+                    num = f_instance([i * dist_num, (i + 1) * dist_num - 1], integral=True)
+                denom = f_instance([i * dist_denom, (i + 1) * dist_denom - 1], integral=True)
+
+                # Calcula theta usando la función arco coseno
+                if denom == 0:
+                    thetal = np.pi / 2 if i * dist_num == 0 else 0
+                else:
+                    thetal = np.arccos(np.sqrt(num / denom))
+
+                # Añade el valor de theta a la sublista
+                thetas.append(thetal)
+
+            # Añade la sublista de valores de theta al resultado
+            result.append(thetas)
+        return result       
